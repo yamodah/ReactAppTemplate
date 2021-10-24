ae2d155b1ac0fce58ece64151fc50e995ef43379
yamodah <yassinedahlek@gmail.com>
2021-10-23 23:16:22 -0400

    multiple functions inside the app function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
-
-function App() {
+function AskWhereSearch (){
   const googSearch = "https://www.google.com/"
   const bingSearch = "https://wwww.bing.com"
   const element = (<p>Hello, would you like to search with<br /><a href={googSearch}>Google</a><br/> or<br /><a href={bingSearch}>Bing</a></p>)
   return element;
+}
+function App() {
+  return( 
+    <>
+  <AskWhereSearch />
+  <AskWhereSearch />
+  </>
+  )
 }
 
 export default App;
