b60e022b1d6b8af52a2c3e372f4c1d17261ab816
yamodah <yassinedahlek@gmail.com>
2021-10-25 10:57:33 -0400

    styled greetings imported btstrap

--- src/ResearchList.js
+++ src/ResearchList.js
@@ -1,14 +1,16 @@
 function ResearchList({list}){
     const listToResearch = list.map(({search,done},index)=>{
-       return (<tr key={index}>
+       return (<tr key={index} className="border">
             <td>{search}</td>
             <td>{done}</td>
         </tr>)
     })
     return <table>
         <thead>
+            <tr className="border">
             <th>SEARCH</th>
             <th>DONE</th>
+            </tr>
         </thead>
         <tbody>
             {listToResearch}
